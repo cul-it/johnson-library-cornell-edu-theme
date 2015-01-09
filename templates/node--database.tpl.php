196c14e246cdbc4b3e8aa4df41a1e210db1e2ab0
Melissa Wallace <mhk33@cornell.edu>
2015-01-09 12:14:04 -0500

    customize template

--- templates/node--database.tpl.php
+++ templates/node--database.tpl.php
@@ -20,21 +20,31 @@
 <?php endif; ?>
 
 <?php if (!empty ($content['field_use'])) : ?>
-  <?php print render($content['field_use']); ?>
+	<div class="db-field db-use">
+  		<?php print render($content['field_use']); ?>
+  	</div>
 <?php endif; ?>
 
 <?php if (!empty ($content['field_address'])) : ?>
-  <?php print render($content['field_address']); ?>
+	<div class="db-field db-connect">
+  		<?php print render($content['field_address']); ?>
+  	</div>
 <?php endif; ?>
 
 <?php if (!empty ($content['field_description'])) : ?>
-  <?php print render($content['field_description']); ?>
+	<div class="db-field db-description">
+  		<?php print render($content['field_description']); ?>
+  	</div>
 <?php endif; ?>
 
 <?php if (!empty ($content['field_moreinfo'])) : ?>
-  <?php print render($content['field_moreinfo']); ?>
+	<div class="db-field db-more-info">
+  		<?php print render($content['field_moreinfo']); ?>
+  	</div>
 <?php endif; ?>
 
 <?php if (!empty ($content['field_availability'])) : ?>
-  <?php print render($content['field_availability']); ?>
+	<div class="db-field db-availability">
+  		<?php print render($content['field_availability']); ?>
+  	</div>
 <?php endif; ?>
