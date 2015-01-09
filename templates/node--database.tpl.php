35c6ec2fa94695fdc68ecb10d332e35f95608881
Melissa Wallace <mhk33@cornell.edu>
2015-01-09 12:20:15 -0500

    customize connect url

--- templates/node--database.tpl.php
+++ templates/node--database.tpl.php
@@ -11,7 +11,7 @@
 
 <?php if (!empty ($content['field_dbaseurl'])) : ?>
 	<div class="db-field db-url">
-  		<?php print render($content['field_dbaseurl']); ?>
+  		<a href="<?php print render($content['field_dbaseurl']); ?>">Connect</a>
   	</div>
 <?php endif; ?>
 
