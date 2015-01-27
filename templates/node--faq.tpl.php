f4819e2b0f2d1bb1264ce0ced8b31cf5ea0b03c8
Melissa Wallace <melissakuo@gmail.com>
2015-01-27 12:50:29 -0500

    format faq node template

---
+++ templates/node--faq.tpl.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728164
+ */
+?>
+
+
+
+<?php if (!empty ($content['field_answer'])) : ?>
+	<div class="faq-field faq-answer">
+  		<?php print render($content['field_answer']); ?>
+  	</div>
+<?php endif; ?>
+
+<?php if (!empty ($content['taxonomy_vocabulary_1'])) : ?>
+  <div class="faq-field faq-list">
+      <?php print render($content['taxonomy_vocabulary_1']); ?>
+  </div>
+<?php endif; ?>
