9457c387c033d24de39b202ef4b1bf175b772497
Melissa Wallace <mhk33@cornell.edu>
2015-03-26 14:30:03 -0400

    move search box into header

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -1,5 +1,5 @@
 <div id="skiptocontent"><a href="#maincontent">skip to main content</a></div>
-<div class="search-collapse collapse visible-xs">
+<div class="search-collapse visible-xs">
   <div class="container">
     <div class="header-search">
       <?php print render($page['search']); ?>
