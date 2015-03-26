082e64bf507ea930a7a41352404d5aa4928c1727
Melissa Wallace <mhk33@cornell.edu>
2015-03-26 15:55:03 -0400

    add template for guides

---
+++ templates/node--guide.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Returns the HTML for a node.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728164
+ */
+?>
+
+
+<?php if (!empty ($content['field_guideurl'])) : ?>
+	<div class="db-field db-url">
+  		<?php print render($content['field_guideurl']); ?>
+  	</div>
+<?php endif; ?>
+
+<?php if (!empty ($content['taxonomy_vocabulary_1'])) : ?>
+  <div class="db-field topic-list">
+      <?php print render($content['taxonomy_vocabulary_1']); ?>
+  </div>
+<?php endif; ?>
